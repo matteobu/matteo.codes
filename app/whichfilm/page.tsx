b5f5156e42c88b62d4eb12b0a59e1beeba868884
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2025-03-19 20:41:35 +0100

    Fix Image preview in whichfilm page

--- app/whichfilm/page.tsx
+++ app/whichfilm/page.tsx
@@ -21,20 +21,29 @@ export default function whichfilm() {
           }}
         >
           <a href="https://whichfilm-xi.vercel.app/">
-            &quot;whichfilm?&quot; Project. Live on: [whichfilm-xi.vercel.app]
-          </a>{' '}
+            &quot;whichfilm?&quot; Project.{' '}
+            <span
+              style={{
+                backgroundColor: 'var(--light-blue)',
+                padding: '0 5px',
+                borderRadius: '4px',
+              }}
+            >
+              Live on: [whichfilm-xi.vercel.app]
+            </span>
+          </a>
         </h2>
         <p className="mb-4">
           <strong>WhichFilm?</strong> is a dynamic web application built with{' '}
-          <strong>Next.js</strong> and powered by the{' '}
-          <strong>Orama search engine</strong>, offering an immersive and
+          <strong>Next.js</strong>, <strong>TypeScript</strong> and powered by
+          the <strong>Orama search engine</strong>, offering an immersive and
           seamless experience for film lovers. It allows users to easily explore
           and discover the perfect indie film from a curated selection of
           hundreds of award-winning movies.
         </p>
         <p className="mb-4">
           With real-time search capabilities, a user-friendly interface, and
-          advanced filtering options, <strong>WhichFilm?</strong> makes it
+          advanced filtering options, <strong>whichfilm?</strong> makes it
           effortless for users to find films based on genres, directors, actors,
           or keywords. Whether you&apos;re a fan of documentaries, thrillers, or
           international cinema, this app helps you quickly find films that match
@@ -57,8 +66,8 @@ export default function whichfilm() {
               Responsive design for a smooth experience across all devices
             </li>
             <li>
-              Built with the power of <strong>Next.js</strong> for fast,
-              scalable performance
+              Built with the power of <strong>Next.js</strong> and{' '}
+              <strong>TypeScript</strong> for fast, scalable performance
             </li>
           </ul>
         </div>
@@ -73,6 +82,11 @@ export default function whichfilm() {
           </a>
           .
         </p>
+        <p className="mb-4 bg-[var(--light-blue)]">
+          Maybe you&apos;re the hiring manager at{' '}
+          <strong className="text-amber-500">JustWatch</strong>, and you&apos;re
+          already thinking, &quot;I need this person on our team!&quot;
+        </p>
       </section>
 
       <section className="mb-12">
@@ -85,7 +99,7 @@ export default function whichfilm() {
         >
           Screenshots
         </h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           <div className="flex justify-center items-center">
             <Image
               src="/whichfilm/whichfilm-home.png"
@@ -106,6 +120,15 @@ export default function whichfilm() {
           </div>
           <div className="flex justify-center items-center">
             <Image
+              src="/whichfilm/whichfilm-film-details.png"
+              alt="WhichFilm Filter"
+              width={400}
+              height={300}
+              className="rounded-lg shadow-lg"
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <Image
               src="/whichfilm/whichfilm-filter.png"
               alt="WhichFilm Filter"
               width={400}
@@ -116,6 +139,15 @@ export default function whichfilm() {
           <div className="flex justify-center items-center">
             <Image
               src="/whichfilm/whichfilm-about.png"
+              alt="WhichFilm About Page"
+              width={400}
+              height={300}
+              className="rounded-lg shadow-lg"
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <Image
+              src="/whichfilm/whichfilm-about-sc.png"
               alt="WhichFilm About Page"
               width={400}
               height={300}
