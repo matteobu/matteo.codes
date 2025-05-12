d5dc9b14a4cf41098cb29d2836976920ffb90856
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2025-05-12 15:11:34 +0200

    Update Intro Pitch

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,14 @@ export default function Home() {
     <section>
       <div className="max-w-screen-xl mx-auto flex items-center justify-between w-full mt-20">
         <p className="mb-4 text-3xl font-extrabold">
-          I am a Software Developer with a strong foundation in JavaScript,
-          TypeScript, React, Next.js and Node.js. Passionate about building
-          scalable, user-friendly applications, I specialize in developing
-          clean, efficient, and maintainable code. With experience in full-stack
-          development, automation, and Agile workflows, I thrive in
-          collaborative environments where innovation and problem-solving drive
-          impactful solutions.
+          I’m a Software Developer who enjoys turning ideas into practical,
+          user-focused applications. I work across the stack using JavaScript
+          (Typescript) and its ecosystem — from front-end frameworks like React
+          and Next.js to back-end tools like Node.js and Nest.js. I focus on
+          building clean, scalable solutions that are easy to maintain and
+          genuinely enjoyable to use. I’m especially drawn to collaborative
+          environments where solving real problems with smart, thoughtful code
+          is the goal.
         </p>
       </div>
       <div className="max-w-screen-xl mx-auto flex items-center justify-between w-full mt-20">
